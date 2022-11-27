450db9c3a082633c68e411fab089e14aab56829e
khh180cm <khh180cm@gmail.com>
2022-11-27 18:02:05 +0900

    Initial Commit

---
+++ TheOneThing/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TheOneThing
+//
+//  Created by 고혁훈 on 2022/11/27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
