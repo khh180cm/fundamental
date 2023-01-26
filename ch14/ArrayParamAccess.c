d88ddcd76e265fff1da424c16bd2b56423cb7a1a
khh180cm <khh180cm@gmail.com>
2023-01-26 09:34:46 +0900

    [ADD] 주소값을 통한 값 변경

---
+++ ch14/ArrayParamAccess.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void AddArrayElem(int * ptr, int len, int elem)
+{
+    for(int i=0; i<len; i++)
+        ptr[i]+=elem;
+}
+
+void ShowArrayElem(int * ptr, int len)
+{
+    for(int i=0; i<len; i++)
+        printf("%d ", ptr[i]);
+    printf("\n");
+}
+
+int main(void)
+{
+    int arr[3]={1, 2, 3};
+    AddArrayElem(arr, sizeof(arr) / sizeof(int), 1);
+    ShowArrayElem(arr, sizeof(arr) / sizeof(int));
+
+    AddArrayElem(arr, sizeof(arr) / sizeof(int), 2);
+    ShowArrayElem(arr, sizeof(arr) / sizeof(int));
+
+    AddArrayElem(arr, sizeof(arr) / sizeof(int), 3);
+    ShowArrayElem(arr, sizeof(arr) / sizeof(int));
+    return 0;
+}
