e1e04297ff851980dc624085f3836c14bd147dd8
khh180cm <khh180cm@gmail.com>
2023-01-28 10:55:27 +0900

    [FIX] 시멘틱한 함수명으로 수정

--- Practice_1.c
+++ Practice_1.c
@@ -1,18 +1,18 @@
 /*
 변수 num에 저장된 값의 제곱을 계산하는 함수를 정의하고,
 이를 호출하는 main함수를 작성하자
-1) Call-by-value 기반의 SquareByValue_1 함수
-2) Call-by-reference 기반의 SquareByValue_2 함수
+1) Call-by-value 기반의 SquareByValue 함수
+2) Call-by-reference 기반의 SquareByReference 함수
 */
 #include <stdio.h>
 
-int SquareByValue_1(int num)
+int SquareByValue(int num)
 {
     // Call-by-value
     return num * num;
 }
 
-void SquareByValue_2(int * ptr)
+void SquareByReference(int * ptr)
 {
     // Call-by-reference
     int origin = *ptr;
@@ -23,9 +23,9 @@ int main(void)
 {
     int num = 2;
 
-    printf("%d \n", SquareByValue_1(num));
+    printf("%d \n", SquareByValue(num));
 
-    SquareByValue_2(&num);
+    SquareByReference(&num);
     printf("%d \n", num);
 
     return 0;
