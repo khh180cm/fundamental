b1668cadc3b3058e782c08cf7d47aeb0d4ddf180
khh180cm <khh180cm@gmail.com>
2023-01-31 09:30:34 +0900

    [ADD] 2차원 배열의 초기화

---
+++ ch16/TwoDimArrayInit.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int arr1[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+    int arr2[3][3] = {
+        {1},
+        {4, 5},
+        {7, 8, 9},
+    };
+    int arr3[3][3]={1, 2, 3, 4, 5, 6, 7};
+
+    // arr1, arr2, arr3 출력
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", arr1[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", arr2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", arr3[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
