89a140267c42c54be325ef6140539bc8ff8cf753
khh180cm <khh180cm@gmail.com>
2023-02-02 09:41:25 +0900

    [FIX] 기능별 함수 단위로 분리 및 리팩터링

--- ch16/Practice_3.c
+++ ch16/Practice_3.c
@@ -1,39 +1,56 @@
+/*
+성적 관리 프로그램을 작성하자.
+과목은 네 과목, 학생도 네 명이다.
+네 사람의 네 과목 점수를 입력을 먼저 받는다.
+그리고 개인별 총점과 과목별 총점을 계산한다.
+결과를 출력한다.
+*/
 #include <stdio.h>
 
-int main(void)
-{
-    int arr[5][5] = {0};
+int arr[5][5];
 
-    // 점수 입력
+void WriteRecord(void)
+{
     for (int i=0; i<4; i++)
     {
+        int sum = 0;
         for (int j=0; j<4; j++)
         {
-            printf("점수 입력: ");
+            printf("%d 과목 점수 : ", j+1);
             scanf("%d", &arr[i][j]);
+            sum += arr[i][j];
         }
+        arr[i][4] = sum;
         printf("\n");
     }
+}
 
-    // 행 총합 계산
+void WriteSumRecord(void)
+{
     for (int i=0; i<4; i++)
     {
+        int sum = 0;
         for (int j=0; j<4; j++)
-            arr[i][4] += arr[i][j];
+            sum += arr[j][i];
+        arr[4][i] = sum;
+        arr[4][4] += sum;
     }
+}
 
-    // 열 총합 계산
-    for (int i=0; i<5; i++)
-    {
-        for (int j=0; j<4; j++)
-            arr[4][i] += arr[j][i];
-    }
-
+void ShowArr(void)
+{
     for (int i=0; i<5; i++)
     {
         for (int j=0; j<5; j++)
-            printf("%d ", arr[i][j]);
+            printf("%3d ", arr[i][j]);
         printf("\n");
     }
+}
+
+int main(void)
+{
+    WriteRecord();
+    WriteSumRecord();
+    ShowArr();
     return 0;
 }
