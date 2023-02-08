9641736cde317eb071331259a24f1db0872a8e61
khh180cm <khh180cm@gmail.com>
2023-02-08 09:33:06 +0900

    [ADD] 삼중 포인터

---
+++ ch17/TripplePointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num = 100;
+    int * ptr = &num;
+    int ** dptr = &ptr;
+    int *** tptr = &dptr;
+
+    printf("%d %d %d \n", *ptr, **dptr, ***tptr);
+
+    return 0;
+}
